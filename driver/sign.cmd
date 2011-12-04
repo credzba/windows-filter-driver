e4fbe18e8b61854b22e4b0b306d938e55cb55d92
Credzba@gmail.com <Credzba@gmail.com@4cca383b-dd84-5eb6-8fbb-9bcb92d39e3b>
2011-12-04 20:41:23 +0000

    Add in sign files

---
+++ driver/sign.cmd
@@ -0,0 +1,4 @@
+echo TEST TEST  TEST  TEST  TEST  TEST  TEST 
+env
+%W7BASE%/bin/amd64/signtool sign /v /s PrivateCertStore /n driver(Test) /t http://timestamp.verisign.com/scripts/timstamp.dll objchk_win7_x86/i386/driver.sys
+echo TEST TEST  TEST  TEST  TEST  TEST  TEST 
